5929454d514cb7002a6d65c3b7bc02d2689db204
SHEFFcode <sheff@sheffmachine.com>
2016-07-31 15:48:06 -0700

    create a custom attribute directive that sets the background color of the selected element to green

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {Highlight} from "./highlight.directive";
 
 @Component({
   moduleId: module.id,
   selector: 'js-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [Highlight]
 })
 export class AppComponent {
 
