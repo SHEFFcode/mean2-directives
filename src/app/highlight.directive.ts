f61472a05c53e3902db67f58cb21456b86002767
SHEFFcode <sheff@sheffmachine.com>
2016-07-31 16:11:38 -0700

    attribute directives with input metadata to set the highlight color from the outside (ie passed into the custom directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,9 +1,21 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class Highlight {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = this.highlightColor;
+  }
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = this.defaultColor;
+  }
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+  @Input() defaultColor = '#fff';
+  @Input('highlight') highlightColor = '#B4D455';
+  private backgroundColor = this.defaultColor;
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
